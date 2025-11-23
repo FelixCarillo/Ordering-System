cb01cf574b7e6874574b5f70487671b9ec03c27d
Felix Carillo <carillo.felix.l21@gmail.com>
2025-11-23 10:03:07 +0800

    Commented the test displays

--- Orderingsystem.java
+++ Orderingsystem.java
@@ -26,53 +26,53 @@ public class Orderingsystem {
     String ContactLensDisposal[] = new String[contacts.length];
     double ContactLensPrices[] = new double[contacts.length];
 
-    // Test display of eyeglasses
-    System.out.println("Available Eyeglasses:");
-    for (int i = 0; i < eyeglasses.length; i++){
-      IDS[i] = eyeglasses[i].getId();
-      Names[i] = eyeglasses[i].getName();
-      Prices[i] = eyeglasses[i].getPrice();
-      Materials[i] = eyeglasses[i].getMaterial();
-      FrameShapes[i] = eyeglasses[i].getFrameType();
-      Colors[i] = eyeglasses[i].getColor();
+    // // Test display of eyeglasses
+    // System.out.println("Available Eyeglasses:");
+    // for (int i = 0; i < eyeglasses.length; i++){
+    //   IDS[i] = eyeglasses[i].getId();
+    //   Names[i] = eyeglasses[i].getName();
+    //   Prices[i] = eyeglasses[i].getPrice();
+    //   Materials[i] = eyeglasses[i].getMaterial();
+    //   FrameShapes[i] = eyeglasses[i].getFrameType();
+    //   Colors[i] = eyeglasses[i].getColor();
 
-      System.out.println(Names[i]);
-      System.out.println(IDS[i]);
-      System.out.println(Names[i]);
-      System.out.println(Prices[i]);
-      System.out.println(Materials[i]);
-      System.out.println(FrameShapes[i]);
-      System.out.println(Colors[i]);
-      System.out.println("-----------------------");
-    }
+    //   System.out.println(Names[i]);
+    //   System.out.println(IDS[i]);
+    //   System.out.println(Names[i]);
+    //   System.out.println(Prices[i]);
+    //   System.out.println(Materials[i]);
+    //   System.out.println(FrameShapes[i]);
+    //   System.out.println(Colors[i]);
+    //   System.out.println("-----------------------");
+    // }
     //Test display for AddOns
-    System.out.println("Available AddOns:");
-    System.out.println(" ");
-    for (int i = 0; i < AddOns.length; i++){
-    	AddOnIDs[i] =AddOns[i].getAddOnID();
-    	AddOnNames[i] =AddOns[i].getAddOnNames();
-    	AddOnPrices[i] =AddOns[i].getAddOnPrices();
+    // System.out.println("Available AddOns:");
+    // System.out.println(" ");
+    // for (int i = 0; i < AddOns.length; i++){
+    // 	AddOnIDs[i] =AddOns[i].getAddOnID();
+    // 	AddOnNames[i] =AddOns[i].getAddOnNames();
+    // 	AddOnPrices[i] =AddOns[i].getAddOnPrices();
 
-      System.out.println(AddOnNames[i]);
-      System.out.println(AddOnIDs[i]);
-      System.out.println(AddOnNames[i]);
-      System.out.println(AddOnPrices[i]);
-      System.out.println("-----------------------");
-    }
+    //   System.out.println(AddOnNames[i]);
+    //   System.out.println(AddOnIDs[i]);
+    //   System.out.println(AddOnNames[i]);
+    //   System.out.println(AddOnPrices[i]);
+    //   System.out.println("-----------------------");
+    // }
      //Test display for Contacts
-    System.out.println("Available Contacts:");
-    System.out.println(" ");
-    for (int i = 0; i < contacts.length; i++){
-    	ContactLensID[i] =contacts[i].getContactLensID();
-    	ContactLensName[i] =contacts[i].getContactLensName();
-    	ContactLensDisposal[i] =contacts[i].getContactLensDisposal();
-    	ContactLensPrices[i] =contacts[i].getContactLensPrices();
+    // System.out.println("Available Contacts:");
+    // System.out.println(" ");
+    // for (int i = 0; i < contacts.length; i++){
+    // 	ContactLensID[i] =contacts[i].getContactLensID();
+    // 	ContactLensName[i] =contacts[i].getContactLensName();
+    // 	ContactLensDisposal[i] =contacts[i].getContactLensDisposal();
+    // 	ContactLensPrices[i] =contacts[i].getContactLensPrices();
 
-      System.out.println(ContactLensName[i]);
-      System.out.println(ContactLensID[i]);
-      System.out.println(ContactLensDisposal[i]);
-      System.out.println(ContactLensPrices[i]);
-      System.out.println("-----------------------");
-    }
+    //   System.out.println(ContactLensName[i]);
+    //   System.out.println(ContactLensID[i]);
+    //   System.out.println(ContactLensDisposal[i]);
+    //   System.out.println(ContactLensPrices[i]);
+    //   System.out.println("-----------------------");
+    // }
   }
 }
